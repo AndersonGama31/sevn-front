d80e5fffd0b1c9711896e65e2f504409568edf3f
AndersonGama31 <andersongama31@gmail.com>
2024-05-23 18:35:15 -0300

    chore: Add ESLint and Next.js configuration files

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+
+const inter = IBM_Plex_Sans({
+subsets: ["latin"],
+weight: ["400", "500", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "SEVN NEWS",
+  description: "The latest news from around the world.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
