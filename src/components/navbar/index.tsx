1bc12718f7d8802d4352f71f81fb9ffb5a146966
AndersonGama31 <andersongama31@gmail.com>
2024-05-26 18:17:07 -0300

    chore: Add hover effect to clickable elements in CSS files

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,22 @@
+'use client'
+
 import React from 'react'
+
+import { ArrowLeft } from 'lucide-react'
+import { usePathname, useRouter } from 'next/navigation'
 
 import styles from './navbar.module.css'
 
 export const Navbar: React.FC = () => {
+  const router = useRouter()
+  const pathname = usePathname()
   return (
     <nav className={styles.container}>
+      {pathname !== '/home' && (
+        <div onClick={() => router.back()}>
+          <ArrowLeft color="white" />
+        </div>
+      )}
       <h1>SEVN NEWS</h1>
     </nav>
   )
